487e546bb6f5c0438ab8e6b6d13f675e7225161a
GUILHERME BARBOSA DE LIMA <126630427+guin1@users.noreply.github.com>
2023-10-10 20:44:18 -0300

    ajustando vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,87 +1,66 @@
-
-
- <script >
-
- 
-
-export default {
-
-  data() {
-    return {
-      num1: 0,
-      num2: 0,
-      operacao: null,
-    }
-  },
-  calculos: {
-    somar: function (num1, num2) {
-      return num1 + num2;
-    },
-    diminuir: function (num1, num2) {
-      return num1 - num2;
-    },
-    dividir: function (num1, num2) {
-      return num1 / num2;
-    },
-    multiplicar: function (num1, num2) {
-      return num1 * num2;
-    },
-
-
-  }
-}
-
- </script>
-
-
 <template>
-  <div class="container">
-    <header class="p-5 mb-4 mt-4 bg-light rounded-3">
+  <div class="container bg-success text-white bg-opacity-85 ">
+    <header class="p-5 mb-4 mt-4 rounded-3">
       <h1>Calculadora de aritmética</h1>
     </header>
-    <form>
+    <form >
       <div class="row">
-
         <div class="col">
-              <input
-              v-model="num1"  type="number" placeholder="Insira um número" required class="form-control" >
-          </div>
-          <div class="col">
-              <input
-              v-model="num2"  type="number" placeholder="Insira um número" required class="form-control" >
-          </div>
+          <input v-model="num1" type="number" placeholder="Insira um número" required class="form-control">
+        </div>
+        <div class="col">
+          <input v-model="num2" type="number" placeholder="Insira um número" required class="form-control">
+        </div>
         <div class="col-md-4">
-            <select class="form-control" v-model="calculos">
-                <option v-on:click="operacao" value="operacao">Selecionar Opções :</option>
-                <option v-on:click="somar" value="soma">Soma</option>
-                <option v-on:click="diminuir" value="subtracao">Subtração</option>
-                <option v-on:click="dividir" value="divisao">Divisão</option>
-                <option v-on:click="multiplicar" value="multiplicacao">multiplicação</option>
-            </select>
+          <select class="form-control" v-model="operacao">
+            <option value="">Selecionar Opção:</option>
+            <option value="soma">Soma</option>
+            <option value="subtracao">Subtração</option>
+            <option value="divisao">Divisão</option>
+            <option value="multiplicacao">Multiplicação</option>
+          </select>
+          <p class="mt-4"> {{ resultado }}</p>
         </div>
       </div>
     </form>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      num1: 0,
+      num2: 0,
+      operacao: "",
+    };
+  },
+  computed: {
+    resultado() {
+      const num1 = parseFloat(this.num1);
+      const num2 = parseFloat(this.num2);
+
+    
+      switch (this.operacao) {
+           case "soma":
+               return num1 + num2;
+            case "subtracao":
+          return num1 - num2;
+        case "divisao":
+          if (num2 !== 0) {
+            return num1 / num2;
+          } else {
+            return "Erro: Divisão por zero";
+          }
+        case "multiplicacao":
+          return num1 * num2;
+        default:
+          return "Resultado :";
+      }
+    },
+  },
+};
+</script>
+
 <style scoped>
 </style>  
-
-
-<!-- 
-somar() {
-  this.operador = (num1, num2) => num1 + num2;
-  this.setarValor();
-},
-dividir() {
-  this.operador = (num1, num2) => num1 / num2;
-  this.setarValor();
-},
-diminuir() {
-  this.operador = (num1, num2) => num1 - num2;
-  this.setarValor();
-},
-multiplicar() {
-  this.operador = (num1, num2) => num1 * num2;
-  this.setarValor();
-} -->
